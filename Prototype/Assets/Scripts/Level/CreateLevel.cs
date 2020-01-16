a8e52de1a42b980cdc92e718f1ed9d0f8b00a693
Paul Bartlett <paulbartlett111@gmail.com>
2020-01-15 20:57:49 -0500

    Initial create level script

--- Prototype/Assets/Scripts/Level/CreateLevel.cs
+++ Prototype/Assets/Scripts/Level/CreateLevel.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CreateLevel : MonoBehaviour
 {
+    public GameObject background;
+    public List<GameObject> backgroundObjs;
+    private Camera mainCamera;
+    private Vector2 screenBounds;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void LoadBackgroundObject(GameObject backgroundObject)
+    {
+        Debug.Log("Loading Object");
     }
 }
